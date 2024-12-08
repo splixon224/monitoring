c8b6adbadac09997b99526c607d7fb8baa4c5211
splixon224 <febriprataman@gmail.com>
2024-12-08 22:49:30 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   content: [
-    './src/**/*.{html,js}', // Menyertakan semua file HTML dan JS di folder src
-    './public/**/*.{html,js}' // Menyertakan semua file HTML dan JS di folder public
+    './src/*.{html,js}', // Menyertakan semua file HTML dan JS di folder src
+    './public/*.{html,js}' // Menyertakan semua file HTML dan JS di folder public
   ],
   theme: {
     extend: {},
