623e4955a3d4800adb79347c7401ea1ef7fc5e96
splixon224 <febriprataman@gmail.com>
2025-01-02 15:02:04 +0700

    change url app script

--- js/system.js
+++ js/system.js
@@ -43,7 +43,7 @@ window.handleSubmit = async (event) => {
 
     const formElement = event.target;
     const formId = formElement.id;
-    const scriptURL = 'https://script.google.com/macros/s/AKfycbxQqFwyoOTXsNnkkiv5vSOLdFoZ_brSd58UchTX6MjMRip_f5pQQtijDqV5AJkYlUt68A/exec'
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbzu7r_dVbocqN5Xvx79tDkx9q7Uc-avTMcK320yYHTz3_xJA-p2Y1Lw2tNRYN663NHqRA/exec'
 
     const btnKirimCI = document.getElementById('btnKirimCI')
     const btnLoadingCI = document.getElementById('btnLoadingCI')
